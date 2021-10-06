63c80b120b4248473e186c5b58e6e313a75a9c09
Hoppa <mohamedehab567t@gmail.com>
2021-10-06 18:33:54 +0200

    seventh commit

--- playstation/models.py
+++ playstation/models.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
-# Teto (used)  : mongodb+srv://admin:99059459@ps1.df6mm.mongodb.net/PS?retryWrites=true&w=majority
-# Sky  : mongodb+srv://admin:99059459@pss.eukti.mongodb.net/PS?retryWrites=true&w=majority
-client = MongoClient("mongodb+srv://admin:99059459@ps1.df6mm.mongodb.net/PS?retryWrites=true&w=majority" , tlsCAFile=ca)
+# Teto : mongodb+srv://admin:99059459@ps1.df6mm.mongodb.net/PS?retryWrites=true&w=majority
+# Sky (used) : mongodb+srv://admin:99059459@pss.eukti.mongodb.net/PS?retryWrites=true&w=majority
+client = MongoClient("mongodb+srv://admin:99059459@pss.eukti.mongodb.net/PS?retryWrites=true&w=majority" , tlsCAFile=ca)
 PS = client.get_database('PS')
 users = PS.get_collection('users')
 services = PS.get_collection('services')
